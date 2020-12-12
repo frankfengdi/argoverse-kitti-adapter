33edf26ab8d91127d91f31a4a6dc0b74fe00dcc4
chenfengxu <chenfengxu714@gmail.com>
2020-12-12 11:08:25 -0800

    downsample data frames, add option to select only one camera or multiple

--- adapter.py
+++ adapter.py
@@ -44,16 +44,36 @@ argodataset
         └──...
 
 """
-
-
 ####CONFIGURATION#################################################
 # Root directory
-root_dir= '/media/msc/8TB/car/argodataset/argoverse-tracking/'
+root_dir= '/mnt/data/argo/argoverse-tracking/'
 
 # Maximum thresholding distance for labelled objects
 # (Object beyond this distance will not be labelled)
-max_d=50
+max_d=70
 
+# When set False, labels out of camera frames are ignored, 
+# Otherwise contain all label 
+need_full_label=False 
+
+# All available cameras
+cams_all = ['ring_front_center',
+ 'ring_front_left',
+ 'ring_front_right',
+ 'ring_rear_left',
+ 'ring_rear_right',
+ 'ring_side_left',
+ 'ring_side_right']
+
+# When set -1, store data from all cameras, otherwise choose only of them
+cam_id = 0
+
+# sample rate to avoid sequential data
+sample_rate = 100
+
+# Setup the root directory 
+data_dir = root_dir+'train1/'
+goal_dir = root_dir+'train1_kitti/'
 ####################################################################
 _PathLike = Union[str, "os.PathLike[str]"]
 def load_ply(ply_fpath: _PathLike) -> np.ndarray:
@@ -71,10 +91,6 @@ def load_ply(ply_fpath: _PathLike) -> np.ndarray:
 
     return np.concatenate((x, y, z), axis=1)
 
-# Setup the root directory 
-
-data_dir=  root_dir+'train_test/'
-goal_dir= root_dir+'train_test_kitti/'
 if not os.path.exists(goal_dir):
     os.mkdir(goal_dir)
     os.mkdir(goal_dir+'velodyne')
@@ -89,23 +105,17 @@ print('\nTotal number of logs:',len(argoverse_loader))
 argoverse_loader.print_all()
 print('\n')
 
-cams=['ring_front_center',
- 'ring_front_left',
- 'ring_front_right',
- 'ring_rear_left',
- 'ring_rear_right',
- 'ring_side_left',
- 'ring_side_right']
 
 
+cams = cams_all if cam_id<0 else [cams_all[cam_id]]
 
 # count total number of files
 total_number=0
 for q in argoverse_loader.log_list:
-	path, dirs, files = next(os.walk(data_dir+q+'/lidar'))
-	total_number= total_number+len(files)
+    path, dirs, files = next(os.walk(data_dir+q+'/lidar'))
+    total_number= total_number+len(files)
 
-total_number= total_number*7
+total_number = total_number*7 if cam_id<0 else total_number
 
 bar = progressbar.ProgressBar(maxval=total_number, \
     widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
@@ -114,7 +124,7 @@ print('Total number of files: {}. Translation starts...'.format(total_number))
 print('Progress:')
 bar.start()
 
-i= 0
+i = 0 # kitti data index 
 
 for log_id in argoverse_loader.log_list:
     argoverse_data= argoverse_loader.get(log_id)
@@ -130,8 +140,6 @@ for log_id in argoverse_loader.log_list:
         for k in calibration_data.extrinsic.reshape(1,16)[0][0:12]:
             L6= L6+ str(k)+ ' '
         L6=L6[:-1]
-
-
         L1='P0: 0 0 0 0 0 0 0 0 0 0 0 0'
         L2='P1: 0 0 0 0 0 0 0 0 0 0 0 0'
         L4='P3: 0 0 0 0 0 0 0 0 0 0 0 0'
@@ -146,12 +154,11 @@ for log_id in argoverse_loader.log_list:
 {}
 {}
      """.format(L1,L2,L3,L4,L5,L6,L7)
-        l=0
-
+        
+        l=0 # data index in a log file
 
         # Loop through the each lidar frame (10Hz) to copy and reconfigure all images, lidars, calibration files, and label files.  
-        for timestamp in argoverse_data.lidar_timestamp_list:
-
+        for timestamp in argoverse_data.lidar_timestamp_list[::sample_rate]:
             # Save lidar file into .bin format under the new directory 
             lidar_file_path= data_dir + log_id + '/lidar/PC_' + str(timestamp) + '.ply'
             target_lidar_file_path= goal_dir + 'velodyne/'+ str(i).zfill(6) + '.bin'
@@ -193,12 +200,11 @@ for log_id in argoverse_loader.log_list:
 
                 center_cam_frame= calibration_data.project_ego_to_cam(np.array([center]))
 
-                if 0<center_cam_frame[0][2]<max_d and 0<image_bbox[0]<1920 and 0<image_bbox[1]<1200 and 0<image_bbox[2]<1920 and  0<image_bbox[3]<1200:
-
-
+                # flag to set bboxes out of image FOV ignored 
+                label_keep = 0<center_cam_frame[0][2]<max_d #and 0<image_bbox[0]<1920 and 0<image_bbox[1]<1200 and 0<image_bbox[2]<1920 and  0<image_bbox[3]<1200
+                
+                if not need_full_label and label_keep or need_full_label:
                     # the center coordinates in cam frame we need for KITTI 
-
-
                     # for the orientation, we choose point 1 and point 5 for application 
                     p1= corners_cam_frame[1]
                     p5= corners_cam_frame[5]
@@ -208,12 +214,48 @@ for log_id in argoverse_loader.log_list:
                     angle= math.atan2(dz,dx)
                     beta= math.atan2(center_cam_frame[0][2],center_cam_frame[0][0])
                     alpha= angle + beta - math.pi/2
-                    line=classes+ ' {} {} {} {} {} {} {} {} {} {} {} {} {} {} \n'.format(round(truncated,2),occulusion,round(alpha,2),round(image_bbox[0],2),round(image_bbox[1],2),round(image_bbox[2],2),round(image_bbox[3],2),round(height,2), round(width,2),round(length,2), round(center_cam_frame[0][0],2),round(center_cam_frame[0][1],2),round(center_cam_frame[0][2],2),round(angle,2))                
+
+                    '''
+                                        #Values    Name      Description
+                    ----------------------------------------------------------------------------
+                       1    type         Describes the type of object: 'Car', 'Van', 'Truck',
+                                         'Pedestrian', 'Person_sitting', 'Cyclist', 'Tram',
+                                         'Misc' or 'DontCare'
+                       1    truncated    Float from 0 (non-truncated) to 1 (truncated), where
+                                         truncated refers to the object leaving image boundaries
+                       1    occluded     Integer (0,1,2,3) indicating occlusion state:
+                                         0 = fully visible, 1 = partly occluded
+                                         2 = largely occluded, 3 = unknown
+                       1    alpha        Observation angle of object, ranging [-pi..pi]
+                       4    bbox         2D bounding box of object in the image (0-based index):
+                                         contains left, top, right, bottom pixel coordinates
+                       3    dimensions   3D object dimensions: height, width, length (in meters)
+                       3    location     3D object location x,y,z in camera coordinates (in meters)
+                       1    rotation_y   Rotation ry around Y-axis in camera coordinates [-pi..pi]
+                       1    score        Only for results: Float, indicating confidence in
+                                         detection, needed for p/r curves, higher is better.
+                    '''
+                    line=classes+ ' {} {} {} {} {} {} {} {} {} {} {} {} {} {} \n'.format(
+                        round(truncated,2),
+                        occulusion,
+                        round(alpha,2),
+                        round(image_bbox[0],2),
+                        round(image_bbox[1],2),
+                        round(image_bbox[2],2),
+                        round(image_bbox[3],2),
+                        round(height,2), 
+                        round(width,2),
+                        round(length,2), 
+                        round(center_cam_frame[0][0],2), # x lateral
+                        round(center_cam_frame[0][1]+height*0.5,2), # y vertical (negative)
+                        round(center_cam_frame[0][2],2), # z longitudinal
+                        round(angle,2))                
 
                     file.write(line)
+
             file.close()
             i+= 1
             if i< total_number:
-            	bar.update(i+1)
+                bar.update(i+1)
 bar.finish()
 print('Translation finished, processed {} files'.format(i))
